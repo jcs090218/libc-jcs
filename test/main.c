4c7c4a98f3bd9858925567eb0304e4b5357a0801
jenchieh <jcs090218@gmail.com>
2017-12-04 15:16:41 +0800

    add test project.

---
+++ test/main.c
@@ -0,0 +1,26 @@
+/**
+ * $File: main.c $
+ * $Date: 2017-12-04 08:01:00 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2017 by Shen, Jen-Chieh $
+ */
+
+#include "../libc_jcs.h"
+
+
+/**
+ * @func main
+ * @brief Program Entry Point.
+ * @param { int } argc : argument count.
+ * @param { char } argv : argument vector.
+ * @return { int } : 0, exit success. -1, exit failure.
+ */
+int main(int argc, char *argv[]) {
+
+    printf("libc-jcs test start..");
+
+    /* --------------- */
+    return 0;
+}
