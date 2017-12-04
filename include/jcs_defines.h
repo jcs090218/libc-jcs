2955ca8fb9d51be0e5f1e5c23dabe4d062260f1f
jenchieh <jcs090218@gmail.com>
2017-12-04 15:18:22 +0800

    add defines.

---
+++ include/jcs_defines.h
@@ -0,0 +1,32 @@
+#ifndef __JCS_DEFINES_H__
+/**
+ * $File: jcs_defines.h $
+ * $Date: 2017-12-04 08:22:55 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2017 by Shen, Jen-Chieh $
+ */
+#define __JCS_DEFINES_H__
+
+
+#include <stdint.h>
+
+
+#define PUBLIC
+#define PRIVATE static
+
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef float float32;
+
+#endif /* __JCS_DEFINES_H__ */
