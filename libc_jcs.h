a4ef30683bc2192dd2b5d04d13a555b1587112b2
jenchieh <jcs090218@gmail.com>
2017-12-04 15:17:17 +0800

    add lib header.

---
+++ libc_jcs.h
@@ -0,0 +1,28 @@
+#ifndef __LIBC_JCS_H__
+/**
+ * $File: libc_jcs.h $
+ * $Date: 2017-12-04 08:07:37 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2017 by Shen, Jen-Chieh $
+ */
+#define __LIBC_JCS_H__
+
+/* Sys */
+#include <sys/stat.h>
+#include <sys/types.h>
+
+/* Standard */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdbool.h>
+
+/* Core */
+#include "./include/jcs_util.h"
+#include "./include/jcs_defines.h"
+
+#endif /* __LIBC_JCS_H__ */
