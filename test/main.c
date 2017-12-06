b82434464f3ff3a79ad70f0d1b3799956e276fa5
jenchieh <jcs090218@gmail.com>
2017-12-06 13:04:39 +0800

    end test read file io.

--- test/main.c
+++ test/main.c
@@ -20,27 +20,7 @@
 int main(int argc, char *argv[]) {
     printf("libc-jcs test start..\n");
 
-    char current_dir[1024];
-    jcs_get_current_dir(current_dir, sizeof(current_dir));
-
-    printf("\n\nCurrent working dir: %s\n\n", current_dir);
-
-
-    char *filePath = "./hello.txt";
-
-    jcs_write_to_file(filePath, "Hello ");
-    jcs_append_to_file(filePath, "World!~\n cool");
-
-    jcs_print_file(filePath);
-
-    char* buffer;
-    buffer = jcs_read_file(filePath);
-
-    printf("\nBuffer content: %s\n", buffer);
-
-    jcs_write_to_file(filePath, buffer);
-
-    free(buffer);
+    
 
     /* --------------- */
     return 0;
