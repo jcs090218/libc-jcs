0b4b680b2ab49813e32f5a2f0763a477d3e26ce0
jenchieh <jcs090218@gmail.com>
2017-12-21 18:14:08 +0800

    use JCSSH package for build command.

--- build.sh
+++ build.sh
@@ -9,10 +9,18 @@
 # ========================================================================
 
 
+# Include all JCSSH files.
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+. "$DIR/scripts/JCSSH/JCSSH.sh"
+
+
 # clean before build.
+jcs_progress_bar_5p 0 "Clean compile status.."
 make realclean
 
 # Build test program.
+jcs_progress_bar_5p 10 "Building executable files.."
 cd test
 echo
 make build
@@ -25,11 +33,13 @@ make buildserver
 cd ..
 
 
+jcs_progress_bar_5p 30 "Building static libraries.."
 echo
 echo
 # Build static library.
 make libc-jcs.a
 
+jcs_progress_bar_5p 40 "Building dynamic libraries.."
 echo
 echo
 # Build dynamic library.
@@ -37,44 +47,57 @@ make libc-jcs.so
 
 
 # Build tar file.
-cho.
+jcs_progress_bar_5p 50 "Start building zip file.."
+
+echo
 echo "Start building the .zip file."
-echo.
+echo
 
-SET PROJECT_NAME=lib-jcs
-SET ZIP_EXT=.tar.gz
-SET ALIB_NAME=libc-jcs.a
-SET SOLIB_NAME=libc-jcs.so
+PROJECT_NAME=lib-jcs
+ZIP_EXT=.tar.gz
+ALIB_NAME=libc-jcs.a
+SOLIB_NAME=libc-jcs.so
 
-mkdir "build/%PROJECT_NAME%"
-mkdir "build/%PROJECT_NAME%/include"
-mkdir "build/%PROJECT_NAME%/lib"
+mkdir "build/$PROJECT_NAME"
+mkdir "build/$PROJECT_NAME/include"
+mkdir "build/$PROJECT_NAME/lib"
 
 echo "Copying library files..."
-cp "./build/alib/%ALIB_NAME%" "./build/%PROJECT_NAME%/lib"
-cp "./build/solib/%SOLIB_NAME%" "./build/%PROJECT_NAME%/lib"
+cp "./build/alib/$ALIB_NAME" "./build/$PROJECT_NAME/lib"
+cp "./build/solib/$SOLIB_NAME" "./build/$PROJECT_NAME/lib"
 echo "Done copying library files."
 
 echo "Copying header file..."
-cp "./libc_jcs.h" "./build/%PROJECT_NAME%/include/"
+cp "./libc_jcs.h" "./build/$PROJECT_NAME/include/"
 echo "Done copying header file."
 
 echo "Copying include files..."
-mkdir "./build/%PROJECT_NAME%/include/include"
-cp -r "./include" "./build/%PROJECT_NAME%/include"
+mkdir "./build/$PROJECT_NAME/include/include"
+cp -r "./include" "./build/$PROJECT_NAME/include"
 echo "Done copying include files."
 
+echo "Copy license file.."
+cp "./LICENSE" "./build/$PROJECT_NAME"
 
+echo "Copy readme file.."
+cp "./README" "./build/$PROJECT_NAME"
+
+jcs_progress_bar_5p 70 "Copying library files..."
+
+
+jcs_progress_bar_5p 80 "Compressing folder...."
 cd build
 echo "Compressing folder..."
-tar -czvf "./%PROJECT_NAME%%ZIP_EXT%" "%PROJECT_NAME%"
+tar -czvf "./$PROJECT_NAME$ZIP_EXT" "$PROJECT_NAME"
 echo "Target files compressed."
 
+jcs_progress_bar_5p 90 "Remove temporary files..."
 echo "Remove temporary files..."
-rm -r "./%PROJECT_NAME%"
+rm -r "./$PROJECT_NAME"
 echo "Temporary files removed."
 cd ..
 
-echo.
+jcs_progress_bar_5p 100 "Done."
+echo
 echo "Done build .zip file."
-echo.
+echo
